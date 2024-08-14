3f2d5d408d31228fc9ff8cdde807a621f1f94959
goldic <goldicstudent@gmail.com>
2024-08-14 22:47:14 +0400

    Add index.css styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { CssBaseline } from '@mui/material'
 import HomePage from './pages/homePage'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CssBaseline />
     <HomePage />
   </StrictMode>
 )
