8d42f3c84c30ae867a455c1b171e226deda7fc3f
goldic <goldicstudent@gmail.com>
2024-08-14 23:11:59 +0400

    Add contacts, about routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -6,5 +6,13 @@ export const routes = [
         path: '/',
         element: <HomePage/>,
         errorElement: <ErrorPage/>   
+    },
+    {
+        path: '/contacts',
+        element: <h1>Contacts page</h1>
+    },
+    {
+        path: '/about',
+        element: <h1>About page</h1>
     }
 ]
