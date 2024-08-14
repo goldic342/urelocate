b298452deb76a99dd3d57b3dd03016e92be42d44
goldic <goldicstudent@gmail.com>
2024-08-14 23:06:54 +0400

    Setup router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import HomePage from './pages/homePage'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { routes } from './routes/routes'
+
+const router = createBrowserRouter(routes)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HomePage />
+    <RouterProvider router={router} />
   </StrictMode>
 )
