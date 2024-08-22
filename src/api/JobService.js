f95cbc08f9f04b952fcfa834ee28990ee38e355d
goldic <goldicstudent@gmail.com>
2024-08-22 17:18:34 +0400

    Change request params, rewrite jsdoc

--- src/api/JobService.js
+++ src/api/JobService.js
@@ -2,27 +2,25 @@ import axios from 'axios'
 
 export default class JobService {
   /**
-   * Retrieves a list of jobs from the server.
-   *
-   * @param {number} [page=1] - The page number of the results to retrieve.
-   * @param {number} [pageSize] - The number of jobs per page.
-   * @param {string} [location] - The location to filter jobs by.
-   * @param {Array} [keywords] - The keywords to filter jobs by.
-   * @param {number} [salary] - The salary range to filter jobs by.
-   * @return {Promise<Object>} - A promise that resolves to the response data.
+   * Fetches a list of jobs from the remote server.
+   * @param {number} [page=1] - The page number to fetch. Defaults to 1.
+   * @param {number} [pageSize=10] - The number of jobs to include in each page.
+   * @param {string} location - The location to search for jobs.
+   * @param {string[]} techStack - The list of technologies to filter by. (Frontend, backend, ML)
+   * @param {string[]} tools - The list of tools like to filter by. (React, Django, python, etc)
+   * @returns {Promise<{jobs: Job[], total: number}>} - A promise that resolves to an object with the jobs list and the total number of jobs.
    */
-  static async getJobs(page = 1, pageSize, location, keywords, salary) {
+  static async getJobs(page = 1, pageSize = 10, location, techStack, tools) {
     const response = await axios.post('http://127.0.0.1/jobs', {
       data: {
         page,
         page_size: pageSize,
         location,
-        salary,
-        keywords: keywords.join(', '),
+        keywords: [...techStack, ...tools].join(', '),
       },
       headers: {
         'Content-Type': 'application/json',
-        'User-Agent': navigator.userAgent,
+        'User-Agent': navigator.userAgent, 
       },
     })
 
